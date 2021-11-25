48ccdcb6251776f07a9e15d822b82eecd97bbb86
Rafael Albuquerque <bzrra.rafael@gmail.com>
2021-11-25 18:43:36 -0300

    Test for basic ERC721 functions

--- test/PartsFactory.test.js
+++ test/PartsFactory.test.js
@@ -54,43 +54,45 @@ contract('PartsFactory', (accounts) => {
     });
 
     //Check transferFrom()
-    it("", async function () {
-
+    it("Should transfer token from account[0] to account[3]", async function () {
+        await truffleAssert.passes(pFactory.safeTransferFrom(ownerAccount, accounts[1], 3));
+        assert.equal(await pFactory.ownerOf(3), accounts[1]);
     });
 
     //Check Transfer event
-    it("", async function () {
-
+    it("Should emit Transfer event", async function () {
+        let _transfer = await pFactory.transferFrom(accounts[1], ownerAccount, 1, {from: accounts[1]});
+        truffleAssert.eventEmitted(_transfer, 'Transfer', (ev) => {
+            return ev.from === accounts[1] && ev.to === ownerAccount && ev.tokenId == 1;
+        })
     });
 
-    //Check approve()
-    it("", async function () {
-        
+    //Check approve() and  getApproved()
+    it("Should approve address to transfer token", async function () {
+        await truffleAssert.passes(pFactory.approve(accounts[2], 1, {from: ownerAccount}));
+        assert.equal(await pFactory.getApproved(1), accounts[2]);
     });
 
     //check Approval event
-    it("", async function () {
-        
+    it("Should emit Aprroval event", async function () {
+        let _approve = await pFactory.approve(accounts[1], 1, {from: ownerAccount});
+        truffleAssert.eventEmitted(_approve, 'Approval', (ev) => {
+            return ev.owner === ownerAccount && ev.approved === accounts[1] && ev.tokenId == 1;
+        })
     });
 
-    //Check setApprovalForAll()
-    it("", async function () {
-        
+    //Check setApprovalForAll() and isApprovedForAll()
+    it("Should approve address for all tokens", async function () {
+        await truffleAssert.passes(pFactory.setApprovalForAll(accounts[2], true, {from: ownerAccount}));
+        assert.equal(await pFactory.isApprovedForAll(ownerAccount, accounts[2]), true);
     });
 
     //check ApprovalForAll event
-    it("", async function () {
-        
-    });
-
-    //Check getApproved()
-    it("", async function () {
-        
-    });
-
-    //check isApprovedForAll()
-    it("", async function () {
-        
+    it("Should emit ApprovedForAll event", async function () {
+        let _setApprovalForAll = await pFactory.setApprovalForAll(accounts[1], true, {from: ownerAccount});
+        truffleAssert.eventEmitted(_setApprovalForAll, 'ApprovalForAll', (ev) => {
+            return ev.owner === ownerAccount && ev.operator === accounts[1] && ev.approved === true;
+        })
     });
 
 })
